ca37e3fce068f19dc2fc445b1724a8952981a9af
hoklaamc <hoklaam.cheng@gmail.com>
2021-04-13 01:36:04 -0400

    WIP compressor UI and implementation

--- src/Compressor.jsx
+++ src/Compressor.jsx
@@ -1,17 +1,62 @@
 import { useState } from "react";
 import { useAudio } from "./helpers";
+import { Slider } from "antd";
 import guitarFile from "./assets/guitar.wav";
+import * as Params from "./params";
 
 const Compressor = () => {
   const [audioFile, setAudioFile] = useState(guitarFile);
   const [isPlaying, toggleIsPlaying] = useAudio(audioFile);
-  const audio = new Audio(audioFile);
+
+  const originalAudio = new Audio(audioFile);
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  const track = audioContext.createMediaElementSource(audio);
-  track.connect(audioContext.destination);
+  const track = audioContext.createMediaElementSource(originalAudio);
+
+  const compressor = audioContext.createDynamicsCompressor();
+  track.connect(compressor);
+  compressor.connect(audioContext.destination);
+
+  const onParamChange = (param, value) => {
+    compressor[param].setValueAtTime(value, audioContext.currentTime);
+    console.dir(compressor[param]);
+  };
 
   return (
-    <button onClick={toggleIsPlaying}>{isPlaying ? "Pause" : "Play"}</button>
+    <>
+      <button onClick={toggleIsPlaying}>{isPlaying ? "Pause" : "Play"}</button>
+      <h4>Reduction</h4>
+      {compressor.reduction}
+      <h4>Threshold</h4>
+      <Slider
+        {...Params.thresholdParams}
+        onChange={(value) => onParamChange("threshold", value)}
+      />
+      <h4>Ratio</h4>
+      <Slider
+        {...Params.ratioParams}
+        onChange={(value) => onParamChange("ratio", value)}
+      />
+      <h4>Knee</h4>
+      <Slider
+        {...Params.kneeParams}
+        onChange={(value) => onParamChange("knee", value * 40)}
+      />
+      <h4>Attack</h4>
+      <Slider
+        {...Params.attackParams}
+        onChange={(value) => onParamChange("attack", value / 1000)}
+      />
+      <h4>Release</h4>
+      <Slider
+        {...Params.releaseParams}
+        onChange={(value) => onParamChange("release", value / 1000)}
+      />
+      <h4>Gain</h4>
+      <Slider
+        {...Params.gainParams}
+        onChange={(value) => onParamChange("gain", value)}
+      />
+    </>
   );
 };
 
---
+++ src/params.js
@@ -0,0 +1,98 @@
+export const thresholdParams = {
+  min: -50,
+  max: 0,
+  step: null,
+  defaultValue: 0,
+  marks: {
+    "-50": "-50dB",
+    "-40": "-40dB",
+    "-30": "-30dB",
+    "-20": "-20dB",
+    "-10": "-10dB",
+    0: "0dB",
+  },
+};
+
+export const ratioParams = {
+  min: 1,
+  max: 20,
+  step: 0.1,
+  marks: {
+    1: "1:1",
+    2: "2:1",
+    3: "3:1",
+    5: "5:1",
+    8: "8:1",
+    12: "12:1",
+    20: "20:1",
+  },
+};
+
+export const kneeParams = {
+  min: 0,
+  max: 1,
+  step: 0.1,
+  marks: {
+    0: "0",
+    0.2: "0.2",
+    0.4: "0.4",
+    0.6: "0.6",
+    0.8: "0.8",
+    1: "1.0",
+  },
+};
+
+export const attackParams = {
+  min: 0,
+  max: 1000,
+  step: 0.5,
+  marks: {
+    0: "0ms",
+    100: "100ms",
+    200: "200ms",
+    300: "300ms",
+    400: "400ms",
+    500: "500ms",
+    600: "600ms",
+    700: "700ms",
+    800: "800ms",
+    900: "900ms",
+    1000: "1000ms",
+  },
+};
+
+export const releaseParams = {
+  min: 0,
+  max: 1000,
+  step: 0.5,
+  marks: {
+    0: "0ms",
+    100: "100ms",
+    200: "200ms",
+    300: "300ms",
+    400: "400ms",
+    500: "500ms",
+    600: "600ms",
+    700: "700ms",
+    800: "800ms",
+    900: "900ms",
+    1000: "1000ms",
+  },
+};
+
+export const gainParams = {
+  min: -20,
+  max: 50,
+  step: 0.5,
+  defaultValue: 0,
+  marks: {
+    "-20": "-20dB",
+    "-10": "-10dB",
+    0: "0dB",
+    10: "10dB",
+    20: "20dB",
+    30: "30dB",
+    40: "40dB",
+    50: "50dB",
+  },
+};
